64e4e2250f4eca42212e3ef39c7424c065f91c34
maicol-W206 <lolmaicoljulian@gmail.com>
2025-09-25 21:03:09 +0000

    segundo camnbio

--- caracteristicas.js
+++ caracteristicas.js
@@ -1,7 +1,24 @@
 async function caracteristicas(){
 var root = document.getElementById("root");
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/picachu`);
+        const res = await fetch('https://pokeapi.co/api/v2/pokemon/' + id);
         const data = await res.json();
+
+        var root = document.getElementById("root");
+         const detalle = `
+    <section class="c-detalle">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png" alt="${data.name}" height="120" width="auto">
+        <p>${data.name}</p>
+        <p>${data.id}</p>
+        <p>Altura: ${data.height / 10} m / Peso: ${data.weight / 10} kg</p>
+        <p>hp: ${data.stats[0].base_stat}</p>
+        <p>Velocidad: ${data.stats[5].base_stat}</p>
+        <p>Ataque: ${data.stats[1].base_stat} Defensa: ${data.stats[2].base_stat}</p>
+        <p>Ataque Especial: ${data.stats[3].base_stat} Defensa Especial: ${data.stats[4].base_stat}</p>
+
+    </section>
+    `;
+
+    root.innerHTML = detalle;
         console.log(data.forms[0].name)
 }
-caracteristicas()
+caracteristicas(5)
