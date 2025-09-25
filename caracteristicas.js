a7c7f0f72f4b7195d6faabd11a693be6f0277271
maicol-W206 <lolmaicoljulian@gmail.com>
2025-09-25 21:27:26 +0000

    tercera subida

--- caracteristicas.js
+++ caracteristicas.js
@@ -4,6 +4,12 @@ var root = document.getElementById("root");
         const data = await res.json();
 
         var root = document.getElementById("root");
+
+        let tipopoke = ``;
+        for (int i = 0; i <data.types.length; i++){
+                tipopoke += `<span>${data.types[i].types.name}</span>`;
+        }
+        root.innerHTML = ""
          const detalle = `
     <section class="c-detalle">
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png" alt="${data.name}" height="120" width="auto">
@@ -14,7 +20,7 @@ var root = document.getElementById("root");
         <p>Velocidad: ${data.stats[5].base_stat}</p>
         <p>Ataque: ${data.stats[1].base_stat} Defensa: ${data.stats[2].base_stat}</p>
         <p>Ataque Especial: ${data.stats[3].base_stat} Defensa Especial: ${data.stats[4].base_stat}</p>
-
+        <p>
     </section>
     `;
 
